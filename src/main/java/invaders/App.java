cf1c8e30efc81c3960c5176731ecd59c9e5b9ba8
gitmoneyfortheboys <habo9157@uni.sydney.edu.au>
2023-11-05 17:56:18 +1100

    Created all difficulties.

--- src/main/java/invaders/App.java
+++ src/main/java/invaders/App.java
@@ -1,11 +1,17 @@
 package invaders;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.input.KeyEvent;
 import invaders.engine.GameEngine;
 import invaders.engine.GameWindow;
 
 public class App extends Application {
+
+    private String selectedDifficulty = "easy"; // default difficulty
 
     public static void main(String[] args) {
         launch(args);
@@ -13,8 +19,43 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        String selectedDifficulty = "easy"; // This should be determined by user input
+        // Create a VBox to hold the instructions
+        VBox root = new VBox();
+        Text instructions = new Text("Press 'E' for Easy, 'M' for Medium, or 'H' for Hard difficulty, then press 'Enter' to start the game.");
+        root.getChildren().add(instructions);
 
+        // Create a scene with the VBox as the root node
+        Scene scene = new Scene(root, 600, 400);
+
+        // Set an event filter for key pressed events
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            switch (event.getCode()) {
+                case E:
+                    selectedDifficulty = "easy";
+                    instructions.setText("Easy difficulty selected. Press 'Enter' to start the game.");
+                    break;
+                case M:
+                    selectedDifficulty = "medium";
+                    instructions.setText("Medium difficulty selected. Press 'Enter' to start the game.");
+                    break;
+                case H:
+                    selectedDifficulty = "hard";
+                    instructions.setText("Hard difficulty selected. Press 'Enter' to start the game.");
+                    break;
+                case ENTER:
+                    startGame(primaryStage);
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        primaryStage.setTitle("Space Invaders - Select Difficulty");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void startGame(Stage primaryStage) {
         // Create the game engine with the selected difficulty
         GameEngine model = new GameEngine(selectedDifficulty);
         GameWindow window = new GameWindow(model);
